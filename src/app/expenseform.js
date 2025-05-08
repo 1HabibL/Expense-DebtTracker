28f05643553f8b79cb1ae37564d4826cc1291752
Habib <50036548+1HabibL@users.noreply.github.com>
2025-05-07 20:16:30 -0400

    implemented filter form buttons

--- src/app/expenseform.js
+++ src/app/expenseform.js
@@ -89,6 +89,7 @@ const clearData = () =>{
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"></link>
           
            <button className="text-white rounded px-4 bg-blue-500 py-3 mt-4" onClick={handleClick}>Add Expense</button>
+            
             <form id="expenseDataForm" className={`my-4 shadow-md rounded p-4 bg-white transform transition-all duration-500 ease-in-out origin-top
      ${isHidden 
       ? 'opacity-0 -translate-y-4 scale-y-95 pointer-events-none max-h-0'
@@ -122,18 +123,17 @@ const clearData = () =>{
                 <button onClick={handleClick} type="submit" className='text-white rounded bg-blue-500 ml-4 px-3 py-1'>Add Expense</button>
                 <button type="button" onClick={handleClick} className='text-white rounded bg-red-500 ml-4 px-3 py-1'>Cancel</button>       
                 </div>
-            
             </form>
             
 
         <div className="my-4 w-99/100 bg-[#f8f9fa] flex flex-col shadow-lg justify-center items-center rounded-lg p-6" id='expenseTextList'>
-        <div className='flex w-99/100'>
-        <h1 className="self-start ml-14.5 mt-4 mb-4 font-semibold text-2xl">Expenses</h1>
-        <button class="ml-auto text-xl mr-14.5 p-2 py-0 bg-gray-300 rounded-lg hover:bg-gray-600" onClick={handleClickFilter} id="resetFilterBtn">filter_alt</button>
+        <div className='flex w-99/100 '>
+        <h1 className="self-start ml-14 mt-4 mb-4 font-semibold text-3xl">Expenses</h1>
+        <button className="ml-auto text-xl mr-14 p-4 my-2 bg-gray-300 rounded-lg hover:bg-gray-600 text-white shadow-md transition flex items-center justify-center" onClick={handleClickFilter} id="resetFilterBtn"><span className="material-symbols-outlined text-2xl">filter_alt</span></button>
         </div>  
 
 
-        <div id="filterFormContainer" className={`bg-gray-200 w-8/10 m-4 
+        <div id="filterFormContainer" className={`bg-gray-200 w-8/10 m-4 flex flex-col items-center 
         ${filterIsHidden
             ? 'opacity-0 -translate-y-4 scale-y-95 pointer-events-none max-h-0'
             : 'opacity-100 translate-y-0 scale-y-100 pointer-events-auto'
@@ -160,6 +160,8 @@ const clearData = () =>{
                 <option value="Rent">Rent</option>
                 <option value="Miscellaneous">Miscellaneous</option>
             </select>
+
+          
             </div>
 
             <div id="dateFilter" className='flex flex-col ml-4 mr-4 mt-1 mb-2'>
@@ -167,6 +169,11 @@ const clearData = () =>{
             <input type="date" className='rounded-l shadow-md bg-white'></input>
             </div>
         </form>
+
+        <div id="filterFormButtons" className='flex flex-row flex-wrap gap-4 mt-4 mb-4'>
+                    <button type="button" className='bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-6 rounded-xl shadow-md hover:shadow-lg transition duration-200'>Apply Filters</button>
+                    <button type="button" className='bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-6 rounded-xl shadow-md hover:shadow-lg transition duration-200'>Reset Filters</button>
+                </div>
         </div> 
             
             <ul className='flex flex-col justify-center items-center w-6/7'>
@@ -176,7 +183,7 @@ const clearData = () =>{
                         <div className="bg-purple-100 flex-1">${expense.amount}</div> 
                         <div className="bg-yellow-100 flex-1">{expense.category}</div> 
                         <div className="text-md text-gray-500 bg-yellow-100 flex-1">{expense.date}</div> 
-                        <button className="bg-red-300 rounded flex-1"onClick={() => handleDelete(index)}><span className="material-symbols-outlined">delete</span></button>
+                        <button className="bg-red-300 text-white p-2 rounded-lg shadow-med hover:bg-red-600 transition flex items-center justify-center"onClick={() => handleDelete(index)}><span className="material-symbols-outlined">delete</span></button>
                     </li>
                     
                 ))}
