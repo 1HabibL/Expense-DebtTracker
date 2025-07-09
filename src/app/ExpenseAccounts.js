126da15e3fafd3fd42fd5a68754d4483ef462b02
Habib <50036548+1HabibL@users.noreply.github.com>
2025-07-09 03:58:59 -0400

    initiated create an expense account feature

--- src/app/ExpenseAccounts.js
+++ src/app/ExpenseAccounts.js
@@ -1,12 +1,30 @@
 "use client"
 
-import React, {UseEffect, UseState} from "react"
+import React, { useEffect, useState } from "react"
 
 function ExpenseAccount(){
+    const [account, setAccounts] = useSate([])
+    const [accountName, setaccountName] = useState("")
+    //Bank Account info
+    const [transitNumber, settransitNumber] = useState("")
+    const [financialInstitution, setfinancialInstitution] = useState("")
+    const [institutionNUmber, setinstitutionNumber] = useState("")
+    //credit card Info
+    const[creditCardNumber, setcreditCardNumber] = useState("")
+    const [credtCardType, setcreditCardType] = useState ("")
 
-    return(<div>
 
-        
+function newAccount = (event) => {
+    event.preventDefault();
+    const passedAccount 
+}
+
+
+
+return(<div>
+   <h1 className="text-2xl font-bold text-gray-800">Expense Accounts</h1>
+      {/* Add account features here */}
+    
     </div>)
 
 } 
